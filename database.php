df05f63e74376bc040cc5ba01b5d94303ee6ecc5
parinya742 <parinyanamkaew48@gmail.com>
2024-07-20 22:45:56 +0700

    edit database to .env

--- database.php
+++ database.php
@@ -1,9 +1,4 @@
 <?php
-// $dbconn = pg_connect("host=localhost port=5432 dbname=sematicadmin user=postgres password=rootpostgres") or die('Could not connect: ' . pg_last_error());
-//                           if (!$dbconn)
-//                           {
-//                               die('Error: Could not connect: ' . pg_last_error());
-//                           }
 
 require 'vendor/autoload.php';
 
